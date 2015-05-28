37bc3e3a781aec42b47a5a63440b2f21ddf77e5e
Gao Dongjing <10302010075@fudan.edu.cn>
2015-05-28 22:56:58 +0800

    EasyClient in TestCase

--- TestEngine/src/testcase/TestCase.java
+++ TestEngine/src/testcase/TestCase.java
@@ -2,9 +2,11 @@ package testcase;
 
 public abstract class TestCase {
 	
+	private EasyClient client;
 	private int threadNum;
 	
-	protected TestCase(int threadNum){
+	protected TestCase(String serverAddress, int threadNum){
+		client = new EasyClient(serverAddress);
 		this.threadNum = threadNum;
 	}
 
@@ -14,6 +16,10 @@ public abstract class TestCase {
 	public abstract void run();
 	
 	public void tearDown() {
+	}
+
+	public EasyClient getClient() {
+		return client;
 	}
 	
 	public int getThreadNum() {
