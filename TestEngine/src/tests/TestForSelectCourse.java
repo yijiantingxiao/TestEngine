9d5736e5667488dced003a61d4073772808f3499
Kid <741383902@qq.com>
2015-07-03 17:26:19 +0800

    test case

--- TestEngine/src/tests/TestForSelectCourse.java
+++ TestEngine/src/tests/TestForSelectCourse.java
@@ -10,6 +10,7 @@ import beans.CourseInfo;
 import beans.SchoolInfo;
 import beans.ServerAnswer;
 import beans.StudentInfo;
+import beans.Time;
 import testcase.TestCase;
 
 public class TestForSelectCourse extends TestCase {
@@ -20,9 +21,11 @@ public class TestForSelectCourse extends TestCase {
 	private String[] cases;
 	private SchoolInfo schoolInfo;
 	private CourseInfo courseInfo;
+	private CourseInfo courseInfo2;
+	private CourseInfo courseInfo3;
 
 	protected TestForSelectCourse(int threadNum) {
-		super(TestVariables.SERVERADDRESS, threadNum, 100);
+		super(TestVariables.SERVERADDRESS, threadNum, 200);
 		random = new Random();
 		standards = new AtomicInteger[6];
 		results = new AtomicInteger[6];
@@ -36,6 +39,7 @@ public class TestForSelectCourse extends TestCase {
 	@Override
 	public boolean setUp() {
 		SchoolInfo schoolInfo = RandomGenerator.getRandomSchoolInfo();
+		schoolInfo.setCreditRequirement(6);
 		ServerAnswer answer = getClient().addSchoolInfo(schoolInfo);
 		if (!checkAnswer(answer)) {
 			System.err.println("setUp @ TestForSelectCourse: addSchoolInfo failed");
@@ -43,12 +47,34 @@ public class TestForSelectCourse extends TestCase {
 		}
 		this.schoolInfo = schoolInfo;
 		CourseInfo courseInfo = RandomGenerator.getRandomCourseInfo(schoolInfo.getSchoolName());
+		courseInfo.setCredit(4);
+		courseInfo.setTime(new Time(1, 1));
 		answer = getClient().addCourseInfo(courseInfo);
 		if (!checkAnswer(answer)) {
-			System.err.println("setUp @ TestForSelectCourse: addCourseInfo failed");
+			System.err.println("setUp @ TestForSelectCourse: addCourseInfo 1 failed");
 			return false;
 		}
 		this.courseInfo = courseInfo;
+		CourseInfo courseInfo2 = RandomGenerator.getRandomCourseInfo(schoolInfo.getSchoolName());
+		courseInfo2.setCredit(4);
+		courseInfo2.setTime(new Time(2, 2));
+		courseInfo2.setCapacity(Integer.MAX_VALUE);
+		answer = getClient().addCourseInfo(courseInfo2);
+		if (!checkAnswer(answer)) {
+			System.err.println("setUp @ TestForSelectCourse: addCourseInfo 2 failed");
+			return false;
+		}
+		this.courseInfo2 = courseInfo2;
+		CourseInfo courseInfo3 = RandomGenerator.getRandomCourseInfo(schoolInfo.getSchoolName());
+		courseInfo3.setCredit(1);
+		courseInfo3.setTime(new Time(1, 1));
+		courseInfo3.setCapacity(Integer.MAX_VALUE);
+		answer = getClient().addCourseInfo(courseInfo3);
+		if (!checkAnswer(answer)) {
+			System.err.println("setUp @ TestForSelectCourse: addCourseInfo 3 failed");
+			return false;
+		}
+		this.courseInfo3 = courseInfo3;
 		return true;
 	}
 
@@ -99,24 +125,74 @@ public class TestForSelectCourse extends TestCase {
 	}
 
 	private void logicForCase2() {
-		
+		standards[1].getAndIncrement();
+		ServerAnswer answer = getClient().selectCourse(courseInfo.getCourseId(), "= =");
+		checkSelectAnswer(answer);
 	}
 	
 	private void logicForCase3() {
-		
+		StudentInfo studentInfo = RandomGenerator.getRandomStudentInfo(schoolInfo.getSchoolName());
+		ServerAnswer answer = getClient().addStudentInfo(studentInfo);
+		if (!checkAnswer(answer)) {
+			System.err.println("logicForCase3 @ TestForSelectCourse: addStudentInfo failed");
+			return;
+		}
+		standards[2].getAndIncrement();
+		answer = getClient().selectCourse("= =", studentInfo.getStudentId());
+		checkSelectAnswer(answer);
 	}
 	
 	private void logicForCase4() {
-		
+		StudentInfo studentInfo = RandomGenerator.getRandomStudentInfo(schoolInfo.getSchoolName());
+		ServerAnswer answer = getClient().addStudentInfo(studentInfo);
+		if (!checkAnswer(answer)) {
+			System.err.println("logicForCase4 @ TestForSelectCourse: addStudentInfo failed");
+			return;
+		}
+		standards[3].getAndIncrement();
+		answer = getClient().selectCourse(courseInfo2.getCourseId(), studentInfo.getStudentId());
+		if (!checkAnswer(answer)) {
+			System.err.println("logicForCase4 @ TestForSelectCourse: selectCourse2 failed");
+			return;
+		}
+		answer = getClient().selectCourse(courseInfo.getCourseId(), studentInfo.getStudentId());
+		checkSelectAnswer(answer);
 	}
 	
 	private void logicForCase5() {
+		StudentInfo studentInfo = RandomGenerator.getRandomStudentInfo(schoolInfo.getSchoolName());
+		ServerAnswer answer = getClient().addStudentInfo(studentInfo);
+		if (!checkAnswer(answer)) {
+			System.err.println("logicForCase5 @ TestForSelectCourse: addStudentInfo failed");
+			return;
+		}
+		standards[4].getAndIncrement();
+		answer = getClient().selectCourse(courseInfo3.getCourseId(), studentInfo.getStudentId());
+		if (!checkAnswer(answer)) {
+			System.err.println("logicForCase5 @ TestForSelectCourse: selectCourse3 failed");
+			return;
+		}
+		answer = getClient().selectCourse(courseInfo.getCourseId(), studentInfo.getStudentId());
+		checkSelectAnswer(answer);
 		
 	}
 	
 	private void checkSelectAnswer(ServerAnswer answer) {
 		if (checkAnswer(answer)) {
 			results[0].getAndIncrement();
+		} else if (!answer.isSuccess()) {
+			String failReason = answer.getFailReason();
+			if (failReason.equals("学生不存在")) {
+				results[1].getAndIncrement();
+			} else if (failReason.equals("课程不存在")) {
+				results[2].getAndIncrement();
+			} else if (failReason.equals("学分已满")) {
+				results[3].getAndIncrement();
+			} else if (failReason.equals("选课时间地点冲突") || failReason.equals("选课时间冲突")) {
+				results[4].getAndIncrement();
+			} else if (failReason.equals("选课人数已满")) {
+				results[5].getAndIncrement();
+			}
 		}
 		
 	}
